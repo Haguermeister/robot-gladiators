dedeffa36060b8913cf8725e5b6231b2c0d1a400
Haguermeister <cadethague70@gmail.com>
2021-12-06 11:57:05 -0500

    Added one round of battle for function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,30 @@
 var player1 = window.prompt("What is your robot's name?");
-console.log(player1);
-//This creates a funtion named "fight"
+var playerhealth = 100;
+var playerattack = 51;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
-//fight();
+console.log("Players name is: " + player1,"Players Health is " + playerhealth,"Platers attack is " + playerattack);
+
+var enemyname = "Roborto";
+var enemyhealth = 50;
+var enemyattack = 12;
+
+var fight =  function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyhealth = enemyhealth - playerattack;
+    console.log(enemyname + "'s health after the attack is: " + enemyhealth + " health points");
+    playerhealth = playerhealth - enemyattack;
+    console.log(player1 + "'s health after the attack is: " + playerhealth+ " health points");
+
+    if(playerhealth <= 0){
+        window.alert(player1 + " has died!");
+    }
+    else if(enemyhealth <= 0){
+        window.alert(enemyname + " has died!");
+    } 
+    else {
+        window.alert(enemyname + " still has" + enemyhealth + " health points left.");
+    };
+
+};
+
+fight();
