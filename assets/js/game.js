b6a8cf6c46473d046fd7c4d56817cb6edf8bfae9
Haguermeister <cadethague70@gmail.com>
2021-12-06 11:29:08 -0500

    Added functions and console.log functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! Javascript is running");
+var player1 = window.prompt("What is your robot's name?");
+console.log(player1);
+//This creates a funtion named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight();
