749b45ff44b164a1055705335f512e8b0dbbd5ee
Haguermeister <cadethague70@gmail.com>
2021-12-08 17:17:12 -0500

    Start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
 var player1 = window.prompt("What is your robot's name?");
 var playerhealth = 100;
-var playerattack = 12;
+var playerattack = 10;
 var playermoney = 10;
 
 console.log("Players name is: " + player1,"Players Health is " + playerhealth,"Platers attack is " + playerattack);
@@ -9,46 +9,88 @@ var enemynames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyhealth = 50;
 var enemyattack = 12;
 
-var fight =  function(enemyname) {
-    window.alert("Welcome to Robot Gladiators!");
+var deadbodies = 0;
 
-    var promptfight = window.prompt("Would you light to FIGHT or Skip this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-    if (promptfight === "fight" || promptfight === "FIGHT") {
+
+var fight =  function(enemyname) {
+    while(enemyhealth > 0 && playerhealth > 0){
+        var promptfight = window.prompt("Would you light to FIGHT or Skip this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+        if(promptfight === "skip" || promptfight === "SKIP") {
+            var confirmskip = window.confirm("Are you sure you want to quit?");
+            if(confirmskip){
+                window.alert(player1 + " has chosen to skip the fight! Goodbye.");
+                playermoney = playermoney - 10;
+                break;
+            }
+        }
         enemyhealth = enemyhealth - playerattack;
         console.log(enemyname + "'s health after the attack is: " + enemyhealth + " health points");
         if(enemyhealth <= 0){
             window.alert(enemyname + " has died!");
+            deadbodies++;
+            break;
         } 
         else {
-            window.alert(enemyname + " still has" + enemyhealth + " health points left.");
+            window.alert(enemyname + " still has " + enemyhealth + " health points left.");
         };
         playerhealth = playerhealth - enemyattack;
         console.log(player1 + "'s health after the attack is: " + playerhealth+ " health points");
         if(playerhealth <= 0){
             window.alert(player1 + " has died!");
+            break;
         }
         else {
             window.alert(player1 + " still has " + playerhealth + " health points left.")
         };
-    } 
-    else if(promptfight === "skip" || promptfight === "SKIP") {
-        var confirmskip = window.confirm("Are you sure you want to quit?");
-        if(confirmskip){
-            window.alert(player1 + " has chosen to skip the fight! Goodbye.");
-            playermoney = playermoney - 2;
+    };
+}
+
+var startgame = function(){
+    playerhealth = 100;
+    playerattack = 10;
+     playermoney = 10;
+
+    for (i=0; i<enemynames.length; i++){
+        if(playerhealth > 0){
+            window.alert("Welcome to Robot Gladiators! Round " + (i +1));
         }
         else {
-            fight()
+            window.alert("You lose! Game Over!");
+            break;
         }
-
-    } 
-    else {
-        window.alert("You need to choose a valid option. Try again!");
-    };
-    
-
-};
-
-for (i=0; i<3; i++){
-    fight(enemynames[i]);
+        pickedenemyname = enemynames[i];
+        enemyhealth=50;
+        fight(pickedenemyname);
+    }
+    thanos();
 }
+
+var thanos = function(){
+    if(deadbodies === 5){
+        window.alert("You win! Opponents defeated = " + deadbodies + " Score = " + playermoney);
+    }
+    else{
+        window.alert("LOSER! Opponents defeated = " + deadbodies + " Score = " + playermoney);
+    }
+    var playagain = window.confirm("Would you like to build a snowman?");
+    if(playagain){
+        startgame();
+    }
+    else{
+        window.alert("GAME OVER!");
+    }
+}
+var shop = function(){
+    var shopanswer = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop? Please enter REFILL ? UPGRADE ? LEAVE to make a choice.");
+    /*if(shopanswer === "REFILL" || "refill"){
+
+    }
+    else if((shopanswer === "UPGRADE" || "upgrade") && playermoney >= 5){
+        playerattack = playerattack + 5;
+        playermoney = playermoney - 5;
+    }
+    else{
+        break;
+    }*/
+}
+startgame();
